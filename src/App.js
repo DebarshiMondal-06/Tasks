8079e740b1172b7f398fecc0ac9161788e163816
Debarshi Mondal <debopiku1122@gmail.com>
2022-04-14 13:49:36 +0530

    initial - added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Button, Layout } from 'antd';
+import { Header } from 'antd/lib/layout/layout';
+import "./index.css";
+import FormComp from './FormComp';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App = () => {
+
+  return <>
+    <Layout>
+      <Header className='navbar shadow-sm'>
+        <div className='text'>
+          ATools
+        </div>
+        <div className='nav-btn'>
+          <Button className='btn btn-startfreetrail'>StartFree Trail</Button>
+          <Button className='btn btn-login'>Login</Button>
+        </div>
+      </Header>
+    </Layout>
+    <Layout>
+      <div className='row login-section'>
+        <section className='col-md-5 login'>
+          <FormComp />
+        </section>
+        <section className='col-md-7 sideimage'>
+          <p className='image'></p>
+        </section>
+      </div>
+    </Layout>
+  </>
 }
 
 export default App;
---
+++ src/FormComp.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Form, Input, Checkbox, Button } from 'antd';
+import { Content } from 'antd/lib/layout/layout';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+
+const FormComp = () => {
+
+  const onFinish = async (values) => {
+    console.log(values);
+    try {
+      const result = await axios({
+        url: "https://reqres.in/api/login",
+        method: 'POST',
+        data: values
+      });
+      console.log(result.data);
+      if (result) {
+        toast.success(`Token:: ${result.data.token}`)
+      }
+    } catch (err) {
+      console.log(err.response);
+      const { error } = err.response && err.response.data;
+      if (error) toast.error(error);
+    }
+  };
+
+
+  return <>
+    <h1>Welcome Back</h1>
+    <p>Lorem ipsum dolor sit amet.</p>
+
+    <Form name="basic" autoComplete="off" onFinish={onFinish} className='form'>
+      <Form.Item name="email">
+        <Input placeholder='Email Address*' className='form-input' />
+      </Form.Item>
+
+      <Form.Item name="password">
+        <Input placeholder='Password*' className='form-input' />
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" className='btn submit-btn' htmlType="submit">
+          Login
+        </Button>
+      </Form.Item>
+
+      <Form.Item className='extras'>
+        <Checkbox className='checkbx'> Remember Password</Checkbox>
+        <Content className='forgot'>Forgot Password?</Content>
+      </Form.Item>
+
+    </Form>
+  </>
+}
+
+export default FormComp;
