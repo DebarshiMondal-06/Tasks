1f0ff815fd2adc5cf1510d323e28deabede3d670
Debarshi Mondal <debopiku1122@gmail.com>
2022-05-13 13:19:09 +0530

    updated_code_actions-08

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,37 +3,33 @@
 echo "hello world"
 
 
-REACTCONFIG="{\
-\"SourceDir\":\"src\",\
-\"DistributionDir\":\"dist\",\
-\"BuildCommand\":\"npm run-script build\",\
-\"StartCommand\":\"npm run-script start\"\
-}"
-AWSCLOUDFORMATIONCONFIG="{\
-\"configLevel\":\"project\",\
-\"useProfile\":false,\
-\"profileName\":\"default\",\
-\"accessKeyId\":\"headlessaccesskeyid\",\
-\"secretAccessKey\":\"headlesssecrectaccesskey\",\
-\"region\":\"us-east-1\"\
-}"
-AMPLIFY="{\
-\"projectName\":\"headlessProjectName\",\
-\"appId\":\"amplifyServiceProjectAppId\",\
-\"envName\":\"myenvname\",\
-\"defaultEditor\":\"code\"\
-}"
-FRONTEND="{\
-\"frontend\":\"javascript\",\
-\"framework\":\"react\",\
-\"config\":$REACTCONFIG\
-}"
-PROVIDERS="{\
-\"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
-}"
+# REACTCONFIG="{\
+# \"SourceDir\":\"src\",\
+# \"DistributionDir\":\"dist\",\
+# \"BuildCommand\":\"npm run-script build\",\
+# \"StartCommand\":\"npm run-script start\"\
+# }"
+# AWSCLOUDFORMATIONCONFIG="{\
+# \"configLevel\":\"project\",\
+# \"useProfile\":false,\
+# \"profileName\":\"default\",\
+# \"accessKeyId\":\"headlessaccesskeyid\",\
+# \"secretAccessKey\":\"headlesssecrectaccesskey\",\
+# \"region\":\"us-east-1\"\
+# }"
+# AMPLIFY="{\
+# \"projectName\":\"headlessProjectName\",\
+# \"appId\":\"amplifyServiceProjectAppId\",\
+# \"envName\":\"myenvname\",\
+# \"defaultEditor\":\"code\"\
+# }"
+# FRONTEND="{\
+# \"frontend\":\"javascript\",\
+# \"framework\":\"react\",\
+# \"config\":$REACTCONFIG\
+# }"
+# PROVIDERS="{\
+# \"awscloudformation\":$AWSCLOUDFORMATIONCONFIG\
+# }"
 
-amplify pull \
---amplify $AMPLIFY \
---frontend $FRONTEND \
---providers $PROVIDERS \
---yes
+amplify pull
